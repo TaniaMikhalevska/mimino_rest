b2afb4d39762901c4dae0168d361f6adb42d9109
Tania_M <askorbinka906@gmail.com>
2018-10-19 00:14:43 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,42 +1,41 @@
 'use strict';
 
 
-/*var data = [
-    {img:'images/hotel/new/10.jpg', title: 'ЛЮКС'},
-    {img:'images/hotel/new/11.jpg', title: 'ЛЮКС'},
-    {img:'images/hotel/new/12.jpg', title: 'СТАНДАРТ'},
-    {img:'images/hotel/new/13.jpg', title: 'ЛЮКС'},
-    {img:'images/hotel/new/14.jpg', title: 'ПОЛУЛЮКС'},
-    {img:'images/hotel/new/15.jpg', title: 'СТАНДАРТ'},
-           ],*/
+
 var data = [
-    {img:'images/hotel/new/10.jpg', title: 'ЛЮКС'},
-    {img:'images/hotel/new/11.jpg', title: 'ЛЮКС'},
-    {img:'images/hotel/new/12.jpg', title: 'СТАНДАРТ'},
-    {img:'images/hotel/new/13.jpg', title: 'ЛЮКС'},
-    {img:'images/hotel/new/14.jpg', title: 'ПОЛУЛЮКС'},
-    {img:'images/hotel/new/15.jpg', title: 'СТАНДАРТ'},
+    {img:'images/hotel/new/10.jpg', title: '«СТАНДАРТ»'},
+    {img:'images/hotel/new/11.jpg', title: '«СТАНДАРТ»'},
+    {img:'images/hotel/new/12.jpg', title: '«СТАНДАРТ»'},
+    {img:'images/hotel/new/13.jpg', title: '«ЛЮКС»'},
+    {img:'images/hotel/new/14.jpg', title: '«ЛЮКС»'},
+    {img:'images/hotel/new/15.jpg', title: '«ЛЮКС»'},
+    {img:'images/hotel/new/16.jpg', title: '«Люкс 2-комнатный»'},
+    {img:'images/hotel/new/17.jpg', title: '«Люкс 2-комнатный»'},
+    {img:'images/hotel/new/18.jpg', title: '«Люкс 2-комнатный»'},
+     {img:'images/hotel/new/19.jpg', title: '«Люкс 2-комнатный»'},
            ],
     newBtn = document.getElementById('new'),
     editableRow =document.getElementById('editableRow');
     
 if (newBtn) {
-    newBtn.addEventListener('click', hideBtn);
+    if ( data.length > 3) {
     newBtn.addEventListener('click', addItem);
-    newBtn.addEventListener('click', returnBtn);
-    newBtn.addEventListener('click', cutData);
+    newBtn.addEventListener('click', cutData); 
+   newBtn.addEventListener('click', returnBtn);}
+    if ( data.length <= 3) {
+       newBtn.addEventListener('click', addItem);
+    newBtn.addEventListener('click', cutData); 
+         newBtn.addEventListener('click', hideBtn); 
+    }
 }
-
 
 function hideBtn() {
-    newBtn.style="display: none";
+   $('#new').remove();
+    /*
+      newBtn.style="display: none";*/ 
 }
 
-if (data.length ==0) {
-     hideBtn();
-}
 function returnBtn() {
-    newBtn.style="display: block";
     editableRow.appendChild(newBtn); 
 }
 function cutData() {
@@ -44,7 +43,7 @@ function cutData() {
 }
 
 function addItem() {
-    var length = data.length < 3 ? data.length : 3;
+    var length = data.length < 4 ? data.length : 3;
     for (var i = 0; i < length; i++) {
     var item = document.createElement('div');
     item.className = 'col-12 col-sm-6 col-xl-4 col-gallery';
@@ -56,30 +55,29 @@ function addItem() {
                     </a>`;
     
     editableRow.appendChild(item);
-        
+        if (data.length<4) {
+            newBtn.addEventListener('click', hideBtn)
+            
+        }
         
         }
+
 }
-
-if (data.length < 1) {
-     hideBtn();
-}
-
-
-
 
 
 
 (function($){
 	$(document).ready(function() {
-        //include
-           $(function(){
+        //include  
     var includes = $('[data-include]');
     $.each(includes, function(){
       var file = 'include/'+$(this).data('include') + '.html';
       $(this).load(file);
     });
-  });
+ 
+        
+        
+        
 		//slider
         $('.hotel__slider').slick({
             infinite: true,
@@ -107,14 +105,58 @@ if (data.length < 1) {
             $('.nav__wrapper').toggleClass('nav__wrapper--active')
         });
        
-        
-        
-        
- 
-        
     })
 
 })(jQuery);
+
+ 
+function initMap() {
+           var center = {lat: 49.599323, lng: 34.525987},
+               map  = new google.maps.Map(document.getElementById('map'), {
+                          zoom: 15, 
+                          center: center,
+                          disableDefaultUI: true,
+                          zoomControl: true,
+                   append:true
+                   
+               }),
+               marker = new google.maps.Marker({
+                        title: 'Ресторан грузинской кухни "Мимино"',
+                        position: {lat: 49.599323, lng: 34.525987},
+                        icon: 'images/marker.png',
+                        map: map
+               }),
+               infowindow =new google.maps.InfoWindow;
+          
+          google.maps.event.addListener(marker, 'click', (function(marker) {
+           var info = '<div class="window">'+'<h1 class="window__title">' +marker.title+'</h1>'+'<p>'+'Добро пожаловать!'+'</p>'+'</div>';
+           return function() {
+           infowindow.setContent(info);
+           infowindow.open(map, marker);
+                    }})(marker));
+
+               };
+google.maps.event.addDomListener(window, 'load', initMap);  
+//modal*/
+google.maps.event.addDomListener(window, "resize", resizingMap());
+
+$('#modal').on('show.bs.modal', function() {
+ 
+   resizeMap();
+})
+
+function resizeMap() {
+   if(typeof map =="undefined") return;
+   setTimeout( function(){resizingMap();} , 2000);
+}
+
+function resizingMap() {
+   if(typeof map =="undefined") return;
+   var center = map.getCenter();
+   google.maps.event.trigger(map, "resize");
+   map.setCenter(center); 
+}
+
 
 
 
